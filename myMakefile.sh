337140ddc1737348b4515f3dacfb456dc99b7f66
Nils Erik Flick (uni) <erik.flick@informatik.uni-hamburg.de>
2011-01-07 13:06:19 +0100

    makefile changes

--- myMakefile.sh
+++ myMakefile.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-CC="ccache g++ -I/opt/intel/ipp/6.1.2.051/ia32/include -Iinclude -Isrc -I/informatik/home/flick/include -I/usr/include -c -fmessage-length=0"
-LD="g++ -L/usr/lib -L/opt/intel/ipp/6.1.2.051/ia32/lib -shared"
+CC="ccache g++ -Iinclude -Isrc -I/informatik/home/flick/include -I/usr/include -c -fmessage-length=0"
+LD="g++ -L/usr/lib -shared"
 
+# Verzeichnisbaum für .o-Dateien erzeugen (weiß der Geier, habe ich von eclipse CDT übernommen)
+for i in $(find src -type d)
+do
+   (mkdir Debug/$i 2>&1 >/dev/null) || true
+done
+
+# Alle .o-Dateien erzeugen
 for i in $(find src -name *.c++)
 do
    echo $i
    ${CC} -O0 -g3 -Wall $i -o Debug/`echo $i | sed s/.c++//`.o
 done
 
+# Linken
 ${LD} $(find Debug -name *.o) -o Debug/libCommonDataAnalysis.so
+
